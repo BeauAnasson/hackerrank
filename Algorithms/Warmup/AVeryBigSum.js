a351c743957e23db29a1cbde919497974009fbbe
Beau Anasson <beauanasson@gmail.com>
2017-06-03 11:08:26 +1000

    A Very Big Sum solved.

---
+++ Algorithms/Warmup/AVeryBigSum.js
@@ -0,0 +1,28 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+let input_stdin = "";
+let input_stdin_array = "";
+let input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+  input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+  input_stdin_array = input_stdin.split('\n');
+  main();
+});
+
+function readLine() {
+  return input_stdin_array[input_currentline++];
+}
+
+function main() {
+  let n = parseInt(readLine());
+  let arr = readLine().split(' ');
+  arr = arr.map(Number);
+
+  let ans = arr.reduce((x, y) => x + y);
+  console.log(ans);
+}
